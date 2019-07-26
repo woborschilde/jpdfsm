66999a8408f9c9a98c36ca2d006649281f2ba7b9
idnaos <riko.woborschil@googlemail.com>
2019-07-26 12:47:36 +0200

    Made UI improvements for best cross-platform fit, Added mergeButtonAvailable logic.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,11 @@
 package sample;
 
+import java.io.File;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
@@ -16,6 +19,7 @@ public class Controller {
     @FXML private TextField txtSelectFile;
     @FXML private TextField txtInputPath;
     @FXML private TextField txtOutputFile;
+    @FXML private Button btnMerge;
 
     private String selectFile = "";
     private String inputPath = "";
@@ -28,19 +32,33 @@ public class Controller {
 
     public void browseSelectorFile() {
         FileChooser fileChooser = new FileChooser();
-        selectFile = fileChooser.showOpenDialog(stage).getAbsoluteFile().toString();
+        File file = fileChooser.showOpenDialog(stage);
+        selectFile = file != null ? file.getAbsoluteFile().toString() : "";
         txtSelectFile.setText(selectFile);
+        mergeButtonAvailable();
     }
 
     public void browseInputPath() {
         DirectoryChooser dirChooser = new DirectoryChooser();
-        inputPath = dirChooser.showDialog(stage).getAbsolutePath() + "\\";
+        File dir = dirChooser.showDialog(stage);
+        inputPath = dir != null ? dir.getAbsolutePath() + "\\" : "";
         txtInputPath.setText(inputPath);
+        mergeButtonAvailable();
     }
 
     public void browseOutputFile() {
         FileChooser fileChooser = new FileChooser();
-        saveFile = fileChooser.showSaveDialog(stage).getAbsoluteFile().toString();
+        File file = fileChooser.showSaveDialog(stage);
+        saveFile = file != null ? file.getAbsoluteFile().toString() : "";
         txtOutputFile.setText(saveFile);
+        mergeButtonAvailable();
+    }
+    
+    private void mergeButtonAvailable() {
+    	if (!txtSelectFile.getText().equals("") && !txtInputPath.getText().equals("") && !txtOutputFile.getText().equals("")) {
+    		btnMerge.setDisable(false);
+    	} else {
+    		btnMerge.setDisable(true);
+    	}
     }
 }
